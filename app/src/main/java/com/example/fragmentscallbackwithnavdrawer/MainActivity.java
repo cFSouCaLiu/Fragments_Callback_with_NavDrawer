3d54c1b374b410e0bd348f25b9fb9e82d94fa02f
Florin_Soucaliuc <fsoucaliuc@outlook.com>
2021-06-10 14:53:24 +0200

    commit du proj "Fragments & CallBack with NavDrawer" juste apres pause dej (les frgms quyi fonct (AM) , puis (PM) juste rajout des meth onStart etc)

--- app/src/main/java/com/example/fragmentscallbackwithnavdrawer/MainActivity.java
+++ app/src/main/java/com/example/fragmentscallbackwithnavdrawer/MainActivity.java
@@ -1,14 +1,142 @@
 package com.example.fragmentscallbackwithnavdrawer;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.fragmentscallbackwithnavdrawer.utils.Gol;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    //la gestion des fragms :
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction ;
+    // creat de la navigview :
+    private NavigationView navigationView ;
+    private static final  String emplacement = MainActivity.class.getSimpleName();
+
+    public void initUi() {
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView =findViewById(R.id.nav_navigationView);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ajout de la meth de init du UI :
+        initUi();
+        // ajout du 1er frgm :
+        addFragment();
+
+        // ajout du support pour la gestion de la toolbar :
+        setSupportActionBar(toolbar);
+        //ajout de la gestion des options d'accessibilite
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(
+                this, // : le contexte
+                drawerLayout, // : le layout de MainActivity
+                toolbar, // : la toolbar
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+        // ajout d un  listener sur le bt hamburger :
+        drawerLayout.addDrawerListener(toogle);
+        toogle.syncState();
+        //
+        navigationView.setNavigationItemSelectedListener(this);
+
+        if(savedInstanceState == null){
+            addFragment();
+            // Force  l affichage du 1er frgm  au demarrage
+            navigationView.setCheckedItem(R.id.nav_fragment_1);
+        }
+
+        // Force  l affichage du 1er frgm  au demarrage
+        // navigationView.setCheckedItem(R.id.nav_fragment_1);
+
+
     }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+        super.onBackPressed();}
+    }
+
+    private void addFragment(){
+        fragmentManager = getSupportFragmentManager();
+        //commencer la discussion :
+        fragmentTransaction = fragmentManager.beginTransaction();
+        // appel de neos frgms :
+        Fragment_01 fragment_01 = new Fragment_01();
+        // ajout au container de fragm :
+        fragmentTransaction.add(R.id.fragment_container, fragment_01);
+        //finalis de la creat du frgm :
+        fragmentTransaction.commit();
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull @org.jetbrains.annotations.NotNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_fragment_1:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Fragment_01()).
+                commit();
+                break;
+            case R.id.nav_fragment_2:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Fragment_02()).
+                        commit();
+                break;
+            case R.id.nav_fragment_3:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Fragment_03()).
+                        commit();
+                break;
+            case R.id.nav_fragment_4:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Fragment_04()).
+                        commit();
+                break;
+            case R.id.nav_fragment_5:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Fragment_05()).
+                        commit();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    protected void onStart(){
+        super.onStart();
+        Gol.addLog(emplacement,"onPause");
+    }
+
+    protected void onResume(){
+        super.onResume();
+        Gol.addLog(emplacement,"onResume");
+    }
+
+    protected void onPause(){
+        super.onPause();
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+
 }
